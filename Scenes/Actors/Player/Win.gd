e02ebc00bbfbd54afcd67ea679890a1a122c8e68
Vikeo <thevikeo@gmail.com>
2022-04-02 17:06:45 +0200

    start adding win state

--- Scenes/Actors/Player/Win.gd
+++ Scenes/Actors/Player/Win.gd
@@ -1,12 +1,9 @@
-extends Node
-
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+#Win state
+extends PlayerState
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Engine.set_target_fps(Engine.get_iterations_per_second())
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
