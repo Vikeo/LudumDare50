8c1dee08ed3a204e151943b4f157923df53a8603
Vikeo <thevikeo@gmail.com>
2022-04-02 16:53:38 +0200

    fix camera

--- Scenes/PlayerCamera.gd
+++ Scenes/PlayerCamera.gd
@@ -2,11 +2,15 @@ extends Camera2D
 
 onready var player : KinematicBody2D = get_parent()
 
-
-
-func _on_StateMachine_transitioned(state_name) -> void:
-	if state_name == "Fall":
-		drag_margin_bottom = 0.0
-		
-	else:
-		drag_margin_bottom = 0.7
+#var target_margin:float = 0.7
+#
+#
+#func _process(delta: float) -> void:
+#	drag_margin_bottom = lerp(drag_margin_bottom, target_margin, delta)
+#
+#func _on_StateMachine_transitioned(state_name) -> void:
+#	if state_name == "Fall":
+#		target_margin = 0.0
+#
+#	else:
+#		target_margin = 0.0
---
+++ Scenes/TestLevel.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Engine.set_target_fps(Engine.get_iterations_per_second())
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
