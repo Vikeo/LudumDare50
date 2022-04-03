5b0c03f03b798baf5745b29597805fd50fbdb1e1
Vikeo <thevikeo@gmail.com>
2022-04-03 23:09:00 +0200

    prettiefy level select

--- LevelSelect.gd
+++ LevelSelect.gd
@@ -2,15 +2,12 @@ extends Control
 
 export (PackedScene) var LevelButton
 export (int) var total_levels = 0
-export (NodePath) var vbox
+export (NodePath) var levelBox
 
 func _ready():
 	Engine.set_target_fps(Engine.get_iterations_per_second())
 	Globals.score = 0
-	vbox = get_node(vbox)
-	
-	if !total_levels <= 4:
-		column_size()
+	levelBox = get_node(levelBox)
 	
 	for i in range(0, total_levels):
 		generate_buttons(i + 1)
@@ -22,10 +19,10 @@ func generate_buttons(name : int):
 	ls.level_path = ("res://Scenes/Levels/" + str(name) + ".tscn")
 	print(ls.level_path)
 	print(ls)
-	vbox.add_child(ls)
+	levelBox.add_child(ls)
 	
 func column_size():
 	if total_levels % 2 == 0:
-		vbox.columns = total_levels/2
+		levelBox.columns = total_levels/2
 	else:
-		vbox.columns = total_levels/2 + 1
+		levelBox.columns = total_levels/2 + 1
