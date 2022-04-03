37a1f5851fbe6ce2e29cf0fd00c5f4dd9f49c47d
Tobias Källkvist <tobias.kallkvist@iths.se>
2022-04-03 18:22:17 +0200

    refactored globals.gd and added functions

--- globals.gd
+++ globals.gd
@@ -4,3 +4,15 @@ var popped : bool = false
 var score = 0
 var final_score = 0
 var balloon_size:float = 0.0
+
+func add_score(amount : int = 1):
+	score += amount
+
+func reset_score() -> void:
+	score = 0
+
+func set_balloon_size(size : float):
+	balloon_size = size
+
+func reset_balloon_size() -> void:
+	balloon_size = 0
